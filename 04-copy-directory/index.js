949f67699d5964a71a28a6c41366d2941fff9785
Marat Nikolaev <gremlen.8149860@yandex.by>
2023-05-04 18:42:41 +0300

    feat: add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const files = path.join(__dirname, 'files');
+const filesCopy = path.join(__dirname, 'files-copy');
 
+fs.rm(filesCopy, {recursive: true, force: true}, (err) => {
+    if (err) throw err;
+
+fs.readdir(files,{withFileTypes: true},(err, files) => {
+    if (err) throw err;
+
+fs.mkdir(filesCopy, {recursive: true, force: true}, err => {
+    if (err) throw err;
+        
+files.forEach(file => {
+fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+    if (err) throw err;
+                    });
+                });
+            }); 
+        }
+    );
+});
